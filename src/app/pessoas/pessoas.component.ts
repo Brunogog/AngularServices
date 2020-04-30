4427ff3576cdf467217ff412926b2f9a497cf13e
Brunogog <bruninhogog@hotmail.com>
2020-04-29 21:42:32 -0300

    inicio

---
+++ src/app/pessoas/pessoas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import [pessoas] from ' ./pessoas.model';
+
+@Component({
+  selector: 'app-pessoas',
+  templateUrl: './pessoas.component.html',
+  styleUrls: ['./pessoas.component.css']
+})
+export class PessoasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
